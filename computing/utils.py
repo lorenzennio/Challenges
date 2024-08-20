7472d417492ca829d805500c7134990bc93e8ba9
Stefan Fröse <stefan.froese@udo.edu>
2024-08-20 12:56:46 +0200

    fix dpi

--- computing/utils.py
+++ computing/utils.py
@@ -6,7 +6,7 @@ import numba as nb
 
 
 def plot_pixels(pixels, figsize=(7, 7), dpi=300, extend=[-2, 1, -3 / 2, 3 / 2]):
-    fig, ax = plt.subplots(figsize=figsize, dpi=300, layout="constrained")
+    fig, ax = plt.subplots(figsize=figsize, dpi=dpi, layout="constrained")
     p = ax.imshow(pixels, extent=extend)
     ax.set_xlabel("x")
     ax.set_ylabel("y")
